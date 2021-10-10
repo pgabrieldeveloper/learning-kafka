d3499fa7a8770aeba2e3ce4028dc72c624732d7e
paulo gabriel <g4brieldev@gmail.com>
2021-10-10 17:17:50 -0300

    criando um produtor

---
+++ src/main/java/KafkaProducerMessage.java
@@ -0,0 +1,20 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class KafkaProducerMessage {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        try(KafkaProducer<String, String> producer = new KafkaProducer<>(properties);) {
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("firsttopicwithjava", "olá kafka");
+            producer.send(record);
+        }
+    }
+}
